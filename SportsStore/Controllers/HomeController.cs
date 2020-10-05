55d155d0ace0b929bba5a33ccd9f71c227208451
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-05 13:56:48 +0100

    Add pagination to HomeController

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
@@ -6,11 +7,18 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage = 1) 
+            => View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
+
+
     }
 }
