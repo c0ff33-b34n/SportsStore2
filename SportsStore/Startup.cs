7c216580aae34d73d3eb0deae2fa7fad269fbbb4
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-06 16:14:53 +0100

    Enable sessions for storing session data in memory

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -24,6 +24,8 @@ namespace SportsStore
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
             services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +34,7 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
