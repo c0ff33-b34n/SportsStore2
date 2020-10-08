4f346738004e5531f9b09411151c771297f5488a
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-08 10:34:22 +0100

    Simplify Cart razor page

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -10,9 +10,10 @@ namespace SportsStore.Pages
     {
         private IStoreRepository repository;
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public Cart Cart { get; set; }
@@ -21,7 +22,6 @@ namespace SportsStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
@@ -30,7 +30,6 @@ namespace SportsStore.Pages
                 .FirstOrDefault(p => p.ProductID == productId);
             Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
